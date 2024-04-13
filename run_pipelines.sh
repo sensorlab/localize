b901dcfa4a3c8feac1e87aac766e29008f71f09f
Gregor Cerar <grega90@gmail.com>
2024-04-13 09:22:55 +0200

    Update

--- run_pipelines.sh
+++ run_pipelines.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Quirk #1: DVC cannot retrieve Lumos5G dataset, because they have expired HTTPS certificate. Let's do it manually.
-lumos_path="./artifacts/lumos5g/data/raw/Lumos5G-v1.0.zip"
-lumos_url="https://lumos5g.umn.edu/downloads/Lumos5G-v1.0.zip"
-if [ ! -e "${lumos_path}" ]; then
-    echo "$lumos_path does not exist. Downloading..."
-    curl --insecure -o ${lumos_path} ${lumos_url}
-fi
-
-
 # TODO: Check if conda environment exists
 
 # Run a command with conda environment without entering.
@@ -16,6 +7,16 @@ conda="conda run -n nancy --no-capture-output"
 
 # The final DVC command
 dvc="${conda} dvc repro --pull"
+
+
+# Quirk #1: DVC cannot retrieve Lumos5G dataset, because they have expired HTTPS certificate. Let's do it manually.
+lumos_path="./artifacts/lumos5g/data/raw/Lumos5G-v1.0.zip"
+lumos_url="https://lumos5g.umn.edu/downloads/Lumos5G-v1.0.zip"
+if [ ! -e "${lumos_path}" ]; then
+    echo "$lumos_path does not exist. Downloading..."
+    eval "${conda} curl --insecure -o ${lumos_path} ${lumos_url}"
+fi
+
 
 for folder in ./configs/*/; do
     if [[ "$folder" != "./configs/common/" ]]; then
